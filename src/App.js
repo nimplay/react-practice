3685b8c480dc0dabe7a04b1842b2388af78c06e8
nimplay <nimrod7day@gmail.com>
2022-08-25 12:56:45 -0400

    create branch

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <div className="App">
       <header className="App-header">        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> 
         </p>
         <a
           className="App-link"
