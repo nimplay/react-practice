e68c09507e6e2e8b418aead944c23597918190f9
nimplay <nimrod7day@gmail.com>
2022-08-26 18:46:15 -0400

    referencies

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import ContadorHooks from './components/ContadorHooks';
 import ScrollHooks from './components/ScrollHooks';
 import RelogHooks from './components/RelogHooks';
 import HooksPersonalizados from './components/HooksPersonalizados';
+import Referencias from './components/Referencias';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">          
         <section>
+        <hr/>
+        <Referencias />
         <hr/>
         <ContadorHooks />
         <hr/>
---
+++ src/components/Referencias.js
@@ -0,0 +1,43 @@
+import React, { createRef, useRef } from "react";
+
+export default function Referencias() {
+  let refMenu = useRef();
+  let refBtn = useRef();
+  const handleToggleMenu = (e) => {
+   /*  const $menu = document.querySelector('#menu');
+    if(e.target.textContent === 'menu') {
+      e.target.textContent = 'cerrar';
+      $menu.style.display = 'block';
+    }else {
+      e.target.textContent = 'menu';
+      $menu.style.display = 'none';
+    } */
+
+    if(refBtn.current.textContent === 'menu') {
+      refBtn.current.textContent = 'cerrar';
+      refMenu.current.style.display = 'block';
+    }else {
+      refBtn.current.textContent = 'menu';
+      refMenu.current.style.display = 'none';
+    } 
+  }
+  return (
+    <>
+      <h1>Referencias</h1>
+      <button id="menu-btn" ref={refBtn} onClick={handleToggleMenu}>menu</button>
+      <nav id="menu" ref={refMenu} style={{display:"none"}}>
+        <a href="#">link</a>
+        <br/>
+        <a href="#">link</a>
+        <br/>
+        <a href="#">link</a>
+        <br/>
+        <a href="#">link</a>
+        <br/>
+        <a href="#">link</a>
+        <br/>
+      </nav>
+
+      
+    </>)
+}
