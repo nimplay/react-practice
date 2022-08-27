05353f6f09fb7706e7c8ca274a59a95e7cfc79c0
nimplay <nimrod7day@gmail.com>
2022-08-27 12:35:23 -0400

    ReactBootstrap MaterialUI

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
-import './App.css';
-import ContadorHooks from './components/ContadorHooks';
-import ScrollHooks from './components/ScrollHooks';
-import RelogHooks from './components/RelogHooks';
-import HooksPersonalizados from './components/HooksPersonalizados';
-import Referencias from './components/Referencias';
-import Formularios from './components/Formularios';
-import Estilos from './components/Estilos';
+import React from 'react';
+/* import ReactBootstrap from './Comp/BootstrapReact'; */
+import MaterialUI from './Comp/MaterialUI';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">          
-        <section>
-        <Estilos />
-        <hr/>
-        <Formularios />
-        <hr/>
-        <Referencias />
-        <hr/>
-        <ContadorHooks />
-        <hr/>
-        <RelogHooks />
-        <hr/>
-        <HooksPersonalizados />
-        <hr/>  
-        <ScrollHooks />
-        <hr/>
-      </section>
-      </header>      
+    <div>
+      <h1>Frameworks CSS con react</h1>      
+     {/*  <ReactBootstrap /> */}
+      <MaterialUI />         
     </div>
   );
 }
---
+++ src/Comp/MaterialUI.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BasicCard from './CardMaterialUI';
+import TemporaryDrawer from './DrawerMaterialUI';
+
+export default function MaterialUI() {
+  return(
+    <>
+    <h2>Material UI</h2>
+    <TemporaryDrawer />
+    <BasicCard />
+    </>
+  )
+}
